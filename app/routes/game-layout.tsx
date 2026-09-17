45ce7c1bb5e466330b2be0d69c54d99e4c9d8f9e
Dawid Brzęczek <navid96@interia.pl>
2026-09-17 23:13:19 +0200

    Refactor code structure for improved readability and maintainability

--- app/routes/game-layout.tsx
+++ app/routes/game-layout.tsx
@@ -9,7 +9,7 @@ export default function GameLayout() {
 			<div className='h-full flex justify-center items-center col-start-1 row-start-1 bg-taupe-400'>
 				<p>Navigation</p>
 			</div>
-			<div className='h-full col-span-4 col-start-2 row-start-1 '>
+			<div className='h-full col-span-4 col-start-2 row-start-1'>
 				<GameHeader />
 			</div>
 			<aside className='h-full row-span-14 row-start-2  bg-blue-800'>
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,7 @@
 import { Button } from '~/components/ui/button'
 import type { Route } from './+types/home'
 import { Player } from '~/game/entities/Player'
-import pergaminTexture from '../../../assets/ui/pergamin-texture.png'
+import pergaminTexture from '../../assets/ui/pergamin-texture.png'
 
 export function meta({}: Route.MetaArgs) {
 	return [{ title: 'New React Router App' }, { name: 'description', content: 'Welcome to React Router!' }]
@@ -12,7 +12,9 @@ export default function Home() {
 	return (
 		<div
 			className='h-full bg-cover bg-center'
-			style={{ backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.65), rgba(255, 255, 255, 0.65)), url(${pergaminTexture})` }}>
+			style={{
+				backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.65), rgba(255, 255, 255, 0.65)), url(${pergaminTexture})`,
+			}}>
 			<Button variant='default' size='lg'>
 				Heal
 			</Button>
